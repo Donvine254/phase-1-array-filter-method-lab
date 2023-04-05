76133d20c42cc788980f86dfad13b815c5f47647
donvine mugendi <donvinemugendi@gmail.com>
2023-04-06 02:09:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(drivers, driver) {
+  const filteredDrivers = drivers.filter(
+    (d) => d.toLowerCase() === driver.toLowerCase()
+  );
+  return filteredDrivers;
+}
+function fuzzyMatch(drivers, driver) {
+  const matchedDrivers = drivers.filter(
+    (d) => d.slice(0, 2) === driver.slice(0, 2)
+  );
+  return matchedDrivers;
+}
+  function matchName(drivers, name) {
+    const matchedDrivers = drivers.filter((driver) => driver.name === name);
+    return matchedDrivers;
+  }
